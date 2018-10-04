a9bf5485b96a76e718083b277f0fa128c1f8360f
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-04 15:07:10 +0100

    WHOOO

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,20 +1,32 @@
 # creating file for docking station class
 require_relative 'bike'
 
+
+
 class DockingStation
 
-  attr_accessor :bike
+  DEFAULT_CAPACITY = 20
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if empty?
+    @arr_bikes.pop
   end
 
   def return_bike(bike)
-    fail 'Station Full' unless station.full
-    @bike = bike
+    fail 'Station full' if full?
+    @arr_bikes << bike
   end
 
-  
+  def initialize
+    @arr_bikes = []
+  end
 
+  private
+  def full?
+     @arr_bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @arr_bikes.empty?
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -16,16 +16,16 @@ describe DockingStation do
   end
 
   describe '#return_bike' do
-    it 'raises error when full' do
-      subject.return_bike(Bike.new)
-      expect { subject.doc Bike.new}.to raise_error 'Docking full'
+    it 'test when station has 20 bikes' do
+      DockingStation::DEFAULT_CAPACITY.times {subject.return_bike Bike.new }
+      expect { subject.return_bike Bike.new }.to raise_error 'Station full'
     end
   end
 
   it 'returns docked bike' do
     bike = Bike.new
-    subject.return_bike(bike)
-    expect(subject.bike).to eq bike
+
+    expect(subject.return_bike(bike)).to eq [bike]
   end
 
   it {is_expected.to respond_to(:return_bike).with(1).argument }
